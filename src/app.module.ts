8bc01ffed44df8fc10f41adc9f5a38a121870b73
Joshua Nwafor <joshuanwafor01@gmail.com>
2023-06-05 13:12:07 +0100

    clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
 import * as path from "path";
-import { CacheModule, MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { MailerModule, HandlebarsAdapter } from "@nest-modules/mailer";
-import { ServeStaticMiddleware } from "@nest-middlewares/serve-static";
-import { MorganModule } from "nest-morgan";
-import { LoggerMiddleware } from "./common/middleware/logger.middleware";
-import { GlobalAccessLogger } from "./common/accessLogger";
-import { AuthModule } from "./auth/auth.module";
-import { UserModule } from "./user/user.module";
+import {
+  CacheModule,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from "@nestjs/common";
+import {MongooseModule} from "@nestjs/mongoose";
+import {MailerModule, HandlebarsAdapter} from "@nest-modules/mailer";
+import {ServeStaticMiddleware} from "@nest-middlewares/serve-static";
+import {MorganModule} from "nest-morgan";
+import {LoggerMiddleware} from "./common/middleware/logger.middleware";
+import {GlobalAccessLogger} from "./common/accessLogger";
+import {AuthModule} from "./auth/auth.module";
+import {UserModule} from "./user/user.module";
 import config from "./config";
-import { CatalogModule } from "./catalog/catalog.module";
-import { JwtModule } from "@nestjs/jwt";
-import { PassportModule } from "@nestjs/passport";
-import { PublicModule } from "./public/public.module";
+import {CatalogModule} from "./catalog/catalog.module";
+import {JwtModule} from "@nestjs/jwt";
+import {PassportModule} from "@nestjs/passport";
+import {PublicModule} from "./public/public.module";
 
 const DEV_TRANSPORTER = {
   host: "smtp-relay.sendinblue.com",
@@ -29,20 +34,20 @@ const DEV_TRANSPORTER = {
     MorganModule,
     PublicModule,
     CatalogModule,
-    MongooseModule.forRoot(config.db??""),
+    MongooseModule.forRoot(config.db ?? ""),
     JwtModule.register({
       secret: "joshua",
-      signOptions: { expiresIn: config.auth.jwtTokenExpireInSec },
+      signOptions: {expiresIn: config.auth.jwtTokenExpireInSec},
     }),
     PassportModule,
     MailerModule.forRootAsync({
       useFactory: () => ({
         transport: DEV_TRANSPORTER,
         defaults: {
-          from:{address:"", name:`${config.mail.from??""}`},
+          from: {address: "", name: `${config.mail.from ?? ""}`},
         },
         template: {
-          dir: __dirname + '/../templates',
+          dir: __dirname + "/../templates",
           adapter: new HandlebarsAdapter(),
           options: {
             strict: true,
@@ -50,7 +55,7 @@ const DEV_TRANSPORTER = {
         },
         options: {
           partials: {
-            dir: path.join(__dirname, 'templates/partials'),
+            dir: path.join(__dirname, "templates/partials"),
             options: {
               strict: true,
             },
--- src/common/enums.ts
+++ src/common/enums.ts
@@ -4,20 +4,15 @@ export enum AccountStatus {
   DISABLED = "DISABLED",
 }
 
-export enum TicketStatus {
-  PENDING = "PENDING",
-  PROCESSING = "PROCESSING",
-  CLOSED = "CLOSED",
-}
 
 export enum AccountType {
   ADMIN = "ADMIN",
-  DRIVER = "DRIVER",
+  DRIVER = "FARMER",
   INDIVIDUAL = "INDIVIDUAL",
 }
 
 export enum SignupAccountType {
-  DRIVER = "DRIVER",
+  DRIVER = "FARMER",
   INDIVIDUAL = "INDIVIDUAL",
 }
 
@@ -28,64 +23,7 @@ export enum ApprovalStatus {
   DISAPPROVED = "DISAPPROVED",
 }
 
-export enum TripInitiatedBy {
-  DRIVER = "DRIVER",
-  PASSENGER = "PASSENGER",
-}
-
-export enum TripStatus {
-  PENDING = "PENDING",
-  STARTED = "STARTED",
-  CLOSED = "CLOSED",
-  COMPLETED = "COMPLETED",
-  CANCELLED = "CANCELLED",
-}
-
-export enum CancellationInitiatedBy {
-  PASSENGER = " PASSENGER",
-  DRIVER = "DRIVER",
-}
-
-export enum RefundType {
-  FULL = "FULL",
-  PARTIAL = "PARTIAL",
-}
-export enum ReservationStatus {
-  PENDING = "PENDING",
-  ACCEPTED = "ACCEPTED",
-  DECLINED = "DECLINED",
-}
-
-export enum PayoutStatus {
-  PENDING = "PENDING",
-  COMPLETED = "COMPLETED",
-  DECLINED = "DECLINED",
-}
-
-export enum PayoutPaymentStatus {
-  INITIATED = "INITIATED",
-  COMPLETED = "COMPLETED",
-  DECLINED = "DECLINED",
-}
-
-export enum PayoutType {
-  SETTLEMENT = "SETTLEMENT",
-  REFUND = "REFUND",
-}
-
 export enum PaymentStatus {
   PENDING = "PENDING",
   COMPLETED = "COMPLETED",
-}
-
-export enum BookingSettlementStatus {
-  COMPLETED = "COMPLETED",
-  PENDING = "PENDING",
-}
-
-export enum BookingStatus {
-  COMPLETED = "COMPLETED",
-  ONBOARDED = "ONBOARDED",
-  PENDING = "PENDING",
-  CANCELLED = "CANCELLED",
 }
