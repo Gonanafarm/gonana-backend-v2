a733d6daa7500f2ea192fc931e6a2b4747c5143e
Rene0115 <enere0115@gmail.com>
2023-10-12 16:04:35 +0100

    verify passcode fix

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -346,11 +346,11 @@ export class UserService extends GenericService<UserDocument> {
     };
   }
 
-  async verifyPasscode(id: string, passcode: string) {
+  async verifyPasscode(email: string, passcode: string) {
     if (passcode.length !== 4) {
       throw new BadRequestException("Passcode must be 4 characters");
     }
-    const user = await this.userModel.findById(id);
+    const user = await this.userModel.findOne({email:email});
     if (!user) {
       throw new NotFoundException(`User not found, login and try again`);
     }
