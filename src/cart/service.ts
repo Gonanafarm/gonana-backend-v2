efb4ebfff696091d90fdb63188b319e3182ed71c
Rene0115 <enere0115@gmail.com>
2023-10-16 00:17:48 +0100

    fix

--- src/cart/service.ts
+++ src/cart/service.ts
@@ -353,9 +353,9 @@ export class CartItemService extends GenericService<CartItemDocument> {
   ) {
     try {
       const rates = await this.getRates(orderItems, user_id, service_code);
-      //@ts-ignore
       const totalCost =
-        parseInt(rates.total_shipping_cost) + rates.product_cost;
+      //@ts-ignore
+      parseInt(rates.total_shipping_cost) + rates.product_cost;
       const user = await this.userModel.findById(user_id);
       if (!user) {
         throw new BadRequestException(`User Not found`);
