021b9a1ad4680c456a25333de59b82d00783584d
Rene0115 <enere0115@gmail.com>
2024-01-24 01:55:36 +0100

    added chain events

--- src/cart/event-handler.ts
+++ src/cart/event-handler.ts
@@ -2,6 +2,8 @@ import {Injectable} from "@nestjs/common";
 import {OnEvent} from "@nestjs/event-emitter";
 import axios from "axios";
 import {CartItemService} from "./service";
+const abi = require("../../abi.json");
+import {ethers} from "ethers";
 
 @Injectable()
 export class CartEventHandler {
@@ -10,14 +12,13 @@ export class CartEventHandler {
   @OnEvent("Products Not Shipped")
   async handleChainEvent(payload: any) {
     console.log("event triggered");
-console.log(payload);
-    
+    console.log(payload);
+
     const res = await axios.post(
       "https://gonana-market.onrender.com/order",
       payload,
     );
     console.log(res.data);
-    
   }
   @OnEvent("Product Shipped")
   async handleEvent(payload: any) {
@@ -30,5 +31,37 @@ console.log(payload);
     );
     console.log(res.data);
   }
-  
+  @OnEvent("Blast Ship Trigger")
+  async handleBlastShipEvent(payload: any) {
+    const marketplaceAddr = "0x686690ef4a57F11A4980e0053E2D1EdD69782F35";
+    const contract = new ethers.Contract(marketplaceAddr, abi, payload.wallet);
+    const order = await contract.orderProduct(
+      payload.productId,
+      payload.amount,
+      payload.buyerId,
+      {
+        value: ethers.utils.parseEther(payload.amount),
+      },
+    );
+    const tx = await order.wait();
+    console.log(order);
+    console.log(tx);
+  }
+
+  @OnEvent("Blast Not Ship Trigger")
+  async handleBlastNotShipEvent(payload: any) {
+    const marketplaceAddr = "0x686690ef4a57F11A4980e0053E2D1EdD69782F35";
+    const contract = new ethers.Contract(marketplaceAddr, abi, payload.wallet);
+    const order = await contract.orderProduct(
+      payload.productId,
+      payload.amount,
+      payload.buyerId,
+      {
+        value: ethers.utils.parseEther(payload.amount),
+      },
+    );
+    const tx = await order.wait();
+    console.log(order);
+    console.log(tx);
+  }
 }
