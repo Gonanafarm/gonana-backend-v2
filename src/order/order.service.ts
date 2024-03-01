41df7c6604e6d0300d2ab3970d52e557726834c8
Rene0115 <enere0115@gmail.com>
2024-03-01 14:51:27 +0100

    fix

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -242,13 +242,15 @@ export class OrderService {
 
         this.userMailerService.farmerOrderCompletedMail(farmerEmail, orderId);
         const farmerPatrons = farmer?.patrons;
-        
+
         console.log(customer?.onesignal_id);
         
         if (farmerPatrons?.includes(customer?.onesignal_id as string)) {
           return;
         } else {
           farmer?.patrons.push(customer?.onesignal_id as string);
+          console.log(farmer?.patrons);
+          
           await farmer?.save();
         }
       }
