85c97674bada472562227711b266afd86be666ff
Rene0115 <enere0115@gmail.com>
2024-03-07 18:04:24 +0100

    commented out cron job

--- src/order/order.cron.ts
+++ src/order/order.cron.ts
@@ -1,10 +1,10 @@
-import {Injectable} from "@nestjs/common";
-import {Cron, CronExpression} from "@nestjs/schedule";
-import * as moment from "moment"
-@Injectable()
-export class OrderCronJob {
-  @Cron(CronExpression.EVERY_30_SECONDS)
-  handleCron() {
-    console.log("Called every 30 seconds");
-  }
-}
+// import {Injectable} from "@nestjs/common";
+// import {Cron, CronExpression} from "@nestjs/schedule";
+// import * as moment from "moment"
+// @Injectable()
+// export class OrderCronJob {
+//   @Cron(CronExpression.EVERY_30_SECONDS)
+//   handleCron() {
+//     console.log("Called every 30 seconds");
+//   }
+// }
