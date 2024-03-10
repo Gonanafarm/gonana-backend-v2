01e835d4a3ff90f1acebb25535c935950a7381bd
Rene0115 <enere0115@gmail.com>
2024-03-10 22:56:23 +0100

    fix

--- src/order/outgoing.order.schema.ts
+++ src/order/outgoing.order.schema.ts
@@ -1,7 +1,6 @@
 import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
 import {ApiProperty} from "@nestjs/swagger";
 import {IsString} from "class-validator";
-import {Moment} from "moment";
 import * as mongoose from "mongoose";
 import {Document} from "mongoose";
 export interface OrderDocument extends Document {
