1a8e2554aa3dc8ebbf1e832a91a80a96c07b226c
Rene0115 <enere0115@gmail.com>
2024-07-26 22:21:02 +0100

    fix

--- src/post/post.event-handler.ts
+++ src/post/post.event-handler.ts
@@ -105,7 +105,7 @@ export class PostEventHandlers {
       "b72cb42b3319abb30fc17f7e20ea58165a84de90c9afd90fcb80382062e01382",
       provider,
     );
-    const marketplaceAddr = "0x99fdeB581d61FDfCF19df461937B9Cc01c18bc2B";
+    const marketplaceAddr = "0x4E4B760e06cbF0b0760279a08b6B836244bc9910";
     const contract = new ethers.Contract(marketplaceAddr, abi, admin);
 
     const list_product = await contract.createProduct(
